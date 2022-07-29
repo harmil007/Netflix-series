45bc95df168d6e3c506de2ba38686e69fdcccc8c
Harry Goti <harrygoti0@gmail.com>
2022-07-29 15:34:40 +0530

    website name

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Netflix from './Netflix.jsx';
-import Amazon from './Amazon.jsx';
+// import Amazon from './Amazon.jsx';
 
-const favserise = "Netflix";
+// const favserise = "Netflix";
  
 
 const App = () => (
    <>
       <h1 className='main__heading'>LIST OF TOP 5 NETFLIX SERIES IN 2022</h1>
       {/* <FavS /> */}
-      {(favserise === "Netflix") ? <Netflix /> :<Amazon />}
+      {/* {(favserise === "Netflix") ? <Netflix /> :<Amazon />} */}
+      <Netflix />
+      {/* <Amazon /> */}
    </>
 );
 
